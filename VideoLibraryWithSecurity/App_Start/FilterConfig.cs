7beb23d1e521e823ff2e83b7bd34000ba8fcd3bd
Simon Burton <burt76@outlook.com>
2019-05-15 17:05:39 +0100

    Add project files.

---
+++ VideoLibraryWithSecurity/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace VideoLibraryWithSecurity
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
